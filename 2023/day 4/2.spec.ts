8ce7b701c424cbf9d982612d92e94c807493b475
Ben Gane <ben.gane@wisetechglobal.com>
2023-12-06 08:37:45 +1100

    Update

--- 2023/day 4/2.spec.ts
+++ 2023/day 4/2.spec.ts
@@ -10,4 +10,25 @@ beforeAll(async () => {
 
 it("works", async () => {
     const lines = input.split("\n");
+    const copies = lines.map(() => 1);
+
+    for (let i = 0; i < lines.length; i++) {
+        const segments = lines[i].split(":");
+        const winningNumbers = segments[1].split("|")[0].trim().split(/\s+/).map((winningNumber) => +winningNumber);
+        const myNumbers = segments[1].split("|")[1].trim().split(/\s+/).map((winningNumber) => +winningNumber);
+        const matchingNumbers = myNumbers.filter(myNumber => winningNumbers.includes(myNumber));
+
+        for (let j = i + 1; j <= Math.min(lines.length - 1, i + matchingNumbers.length); j++) {
+            copies[j] += copies[i];
+        }
+    }
+
+    const total = copies.reduce((accumulator, count) => accumulator + count, 0);
+    console.log(total);
 });
+
+/*
+[1]
+[1, 2, 2, 2, 2, 1]
+[1, 2, 4, 4, 2, 1]
+*/
