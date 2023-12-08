1dcf986eeef9b704240aed5b34154696c7dc11c1
Ben Gane <ben.gane@wisetechglobal.com>
2023-12-09 09:57:53 +1100

    Update

--- challenges/day-8/2.spec.ts
+++ challenges/day-8/2.spec.ts
@@ -4,21 +4,24 @@ import { it } from "vitest";
 
 type Data = { instructions: string, directions: Map<string, [string, string]> };
 
+const isStartingNode = (node: string) => node.endsWith('A');
+const isFinishingNode = (node: string) => node.endsWith('Z');
+
+const getDirectionIndex = (instruction: string) => instruction === 'L' ? 0 : 1;
 const getGreatestCommonDivisor = (a: number, b: number) =>  b === 0 ? a : getGreatestCommonDivisor(b, a % b);
 const getLeastCommonMultiple = (a: number, b: number) => a * b / getGreatestCommonDivisor(a, b);
 
 const computeSteps = ({ instructions, directions }: Data) => {
-  const cursors = [...directions.keys()].filter((key) => key.endsWith('A'));
+  const cursors = [...directions.keys()].filter((key) => isStartingNode(key));
   const minimumDistances = cursors.map(() => Infinity);
 
   let steps = 0;
+  
   while (minimumDistances.includes(Infinity)) {
-    const directionIndex = instructions[steps % instructions.length] === 'L' ? 0 : 1;
+    const directionIndex = getDirectionIndex(instructions[steps % instructions.length]);
     for (let i = 0; i < cursors.length; i++) {
       cursors[i] = directions.get(cursors[i])![directionIndex];
-      if (cursors[i].endsWith('Z')) {
-        minimumDistances[i] = Math.min(minimumDistances[i], steps + 1);
-      }
+      minimumDistances[i] = isFinishingNode(cursors[i]) ? Math.min(minimumDistances[i], steps + 1) : minimumDistances[i];
     }
     steps++;
   }
