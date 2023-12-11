5ff23c857e577e9011977676d3025dbbe76ae2a7
Ben Gane <ben.gane@wisetechglobal.com>
2023-12-11 22:03:18 +1100

    Clean up

--- challenges/day-11/code.ts
+++ challenges/day-11/code.ts
@@ -2,35 +2,35 @@ export type Data = string[][];
 
 const galaxy = "#";
 
-const getExpandableRowsAndCols = (data: Data) => {
-  const expandableRows = new Set<number>(
+const getExpandedRowsAndCols = (data: Data) => {
+  const expandedRows = new Set<number>(
     data.map((_, i) => i).filter((i) => !data[i].includes(galaxy)),
   );
 
-  const expandableCols = new Set<number>(
+  const expandedCols = new Set<number>(
     data[0]
       .map((_, i) => i)
       .filter((i) => data.every((row) => row[i] !== galaxy)),
   );
 
-  return [expandableRows, expandableCols];
+  return [expandedRows, expandedCols];
 };
 
 const getShortestPath = (
   [rowA, colA]: number[],
   [rowB, colB]: number[],
-  expandableRows: Set<number>,
-  expandableCols: Set<number>,
+  expandedRows: Set<number>,
+  expandedCols: Set<number>,
   expansionFactor: number,
 ) => {
   [rowA, rowB] = [Math.min(rowA, rowB), Math.max(rowA, rowB)];
   [colA, colB] = [Math.min(colA, colB), Math.max(colA, colB)];
 
-  const rowDilationFactor = [...expandableRows].filter(
+  const rowDilationFactor = [...expandedRows].filter(
     (row) => row > rowA && row < rowB,
   ).length;
 
-  const colDilationFactor = [...expandableCols].filter(
+  const colDilationFactor = [...expandedCols].filter(
     (col) => col > colA && col < colB,
   ).length;
 
@@ -49,7 +49,7 @@ export const getSumOfShortestPaths = (data: Data, expansionFactor: number) => {
     .flatMap((row, i) => row.map((_, j) => [i, j]))
     .filter(([i, j]) => data[i][j] === galaxy);
 
-  const [expandableRows, expandableCols] = getExpandableRowsAndCols(data);
+  const [expandedRows, expandedCols] = getExpandedRowsAndCols(data);
 
   let sum = 0;
   for (let i = 0; i < galaxies.length; i++) {
@@ -57,8 +57,8 @@ export const getSumOfShortestPaths = (data: Data, expansionFactor: number) => {
       sum += getShortestPath(
         galaxies[i],
         galaxies[j],
-        expandableRows,
-        expandableCols,
+        expandedRows,
+        expandedCols,
         expansionFactor,
       );
     }
